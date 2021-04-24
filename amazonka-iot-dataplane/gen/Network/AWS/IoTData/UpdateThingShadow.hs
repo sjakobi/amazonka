a4f736a0e796f31374ffaa64332c923e7a52b3a7
Brendan Hay <brendan.g.hay@gmail.com>
2021-04-24 10:59:21 +0200

    Updating and regenerating amazonka-iot-dataplane

--- amazonka-iot-dataplane/gen/Network/AWS/IoTData/UpdateThingShadow.hs
+++ amazonka-iot-dataplane/gen/Network/AWS/IoTData/UpdateThingShadow.hs
@@ -1,47 +1,47 @@
 {-# LANGUAGE DeriveDataTypeable #-}
-{-# LANGUAGE DeriveGeneric      #-}
-{-# LANGUAGE OverloadedStrings  #-}
-{-# LANGUAGE RecordWildCards    #-}
-{-# LANGUAGE TypeFamilies       #-}
-
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# OPTIONS_GHC -fno-warn-unused-binds #-}
 {-# OPTIONS_GHC -fno-warn-unused-imports #-}
-{-# OPTIONS_GHC -fno-warn-unused-binds   #-}
 {-# OPTIONS_GHC -fno-warn-unused-matches #-}
 
 -- Derived from AWS service descriptions, licensed under Apache 2.0.
 
 -- |
 -- Module      : Network.AWS.IoTData.UpdateThingShadow
--- Copyright   : (c) 2013-2018 Brendan Hay
+-- Copyright   : (c) 2013-2021 Brendan Hay
 -- License     : Mozilla Public License, v. 2.0.
 -- Maintainer  : Brendan Hay <brendan.g.hay+amazonka@gmail.com>
 -- Stability   : auto-generated
 -- Portability : non-portable (GHC extensions)
 --
--- Updates the thing shadow for the specified thing.
+-- Updates the shadow for the specified thing.
 --
 --
--- For more information, see <http://docs.aws.amazon.com/iot/latest/developerguide/API_UpdateThingShadow.html UpdateThingShadow> in the /AWS IoT Developer Guide/ .
---
+-- For more information, see <http://docs.aws.amazon.com/iot/latest/developerguide/API_UpdateThingShadow.html UpdateThingShadow> in the AWS IoT Developer Guide.
 module Network.AWS.IoTData.UpdateThingShadow
-    (
-    -- * Creating a Request
-      updateThingShadow
-    , UpdateThingShadow
+  ( -- * Creating a Request
+    updateThingShadow,
+    UpdateThingShadow,
+
     -- * Request Lenses
-    , utsThingName
-    , utsPayload
+    utsShadowName,
+    utsThingName,
+    utsPayload,
 
     -- * Destructuring the Response
-    , updateThingShadowResponse
-    , UpdateThingShadowResponse
+    updateThingShadowResponse,
+    UpdateThingShadowResponse,
+
     -- * Response Lenses
-    , utsrsPayload
-    , utsrsResponseStatus
-    ) where
+    utsrrsPayload,
+    utsrrsResponseStatus,
+  )
+where
 
 import Network.AWS.IoTData.Types
-import Network.AWS.IoTData.Types.Product
 import Network.AWS.Lens
 import Network.AWS.Prelude
 import Network.AWS.Request
@@ -53,59 +53,74 @@ import Network.AWS.Response
 --
 -- /See:/ 'updateThingShadow' smart constructor.
 data UpdateThingShadow = UpdateThingShadow'
-  { _utsThingName :: !Text
-  , _utsPayload   :: !ByteString
-  } deriving (Eq, Show, Data, Typeable, Generic)
-
+  { _utsShadowName ::
+      !(Maybe Text),
+    _utsThingName :: !Text,
+    _utsPayload :: !ByteString
+  }
+  deriving (Eq, Show, Data, Typeable, Generic)
 
 -- | Creates a value of 'UpdateThingShadow' with the minimum fields required to make a request.
 --
 -- Use one of the following lenses to modify other fields as desired:
 --
+-- * 'utsShadowName' - The name of the shadow.
+--
 -- * 'utsThingName' - The name of the thing.
 --
 -- * 'utsPayload' - The state information, in JSON format.
-updateThingShadow
-    :: Text -- ^ 'utsThingName'
-    -> ByteString -- ^ 'utsPayload'
-    -> UpdateThingShadow
+updateThingShadow ::
+  -- | 'utsThingName'
+  Text ->
+  -- | 'utsPayload'
+  ByteString ->
+  UpdateThingShadow
 updateThingShadow pThingName_ pPayload_ =
-  UpdateThingShadow' {_utsThingName = pThingName_, _utsPayload = pPayload_}
+  UpdateThingShadow'
+    { _utsShadowName = Nothing,
+      _utsThingName = pThingName_,
+      _utsPayload = pPayload_
+    }
 
+-- | The name of the shadow.
+utsShadowName :: Lens' UpdateThingShadow (Maybe Text)
+utsShadowName = lens _utsShadowName (\s a -> s {_utsShadowName = a})
 
 -- | The name of the thing.
 utsThingName :: Lens' UpdateThingShadow Text
-utsThingName = lens _utsThingName (\ s a -> s{_utsThingName = a})
+utsThingName = lens _utsThingName (\s a -> s {_utsThingName = a})
 
 -- | The state information, in JSON format.
 utsPayload :: Lens' UpdateThingShadow ByteString
-utsPayload = lens _utsPayload (\ s a -> s{_utsPayload = a})
+utsPayload = lens _utsPayload (\s a -> s {_utsPayload = a})
 
 instance AWSRequest UpdateThingShadow where
-        type Rs UpdateThingShadow = UpdateThingShadowResponse
-        request = postBody ioTData
-        response
-          = receiveBytes
-              (\ s h x ->
-                 UpdateThingShadowResponse' <$>
-                   (pure (Just x)) <*> (pure (fromEnum s)))
+  type Rs UpdateThingShadow = UpdateThingShadowResponse
+  request = postBody ioTData
+  response =
+    receiveBytes
+      ( \s h x ->
+          UpdateThingShadowResponse'
+            <$> (pure (Just x)) <*> (pure (fromEnum s))
+      )
 
-instance Hashable UpdateThingShadow where
+instance Hashable UpdateThingShadow
 
-instance NFData UpdateThingShadow where
+instance NFData UpdateThingShadow
 
 instance ToBody UpdateThingShadow where
-        toBody = toBody . _utsPayload
+  toBody = toBody . _utsPayload
 
 instance ToHeaders UpdateThingShadow where
-        toHeaders = const mempty
+  toHeaders = const mempty
 
 instance ToPath UpdateThingShadow where
-        toPath UpdateThingShadow'{..}
-          = mconcat ["/things/", toBS _utsThingName, "/shadow"]
+  toPath UpdateThingShadow' {..} =
+    mconcat ["/things/", toBS _utsThingName, "/shadow"]
 
 instance ToQuery UpdateThingShadow where
-        toQuery = const mempty
+  toQuery UpdateThingShadow' {..} =
+    mconcat ["name" =: _utsShadowName]
 
 -- | The output from the UpdateThingShadow operation.
 --
@@ -113,32 +128,37 @@ instance ToQuery UpdateThingShadow where
 --
 -- /See:/ 'updateThingShadowResponse' smart constructor.
 data UpdateThingShadowResponse = UpdateThingShadowResponse'
-  { _utsrsPayload        :: !(Maybe ByteString)
-  , _utsrsResponseStatus :: !Int
-  } deriving (Eq, Show, Data, Typeable, Generic)
-
+  { _utsrrsPayload ::
+      !(Maybe ByteString),
+    _utsrrsResponseStatus ::
+      !Int
+  }
+  deriving (Eq, Show, Data, Typeable, Generic)
 
 -- | Creates a value of 'UpdateThingShadowResponse' with the minimum fields required to make a request.
 --
 -- Use one of the following lenses to modify other fields as desired:
 --
--- * 'utsrsPayload' - The state information, in JSON format.
+-- * 'utsrrsPayload' - The state information, in JSON format.
 --
--- * 'utsrsResponseStatus' - -- | The response status code.
-updateThingShadowResponse
-    :: Int -- ^ 'utsrsResponseStatus'
-    -> UpdateThingShadowResponse
+-- * 'utsrrsResponseStatus' - -- | The response status code.
+updateThingShadowResponse ::
+  -- | 'utsrrsResponseStatus'
+  Int ->
+  UpdateThingShadowResponse
 updateThingShadowResponse pResponseStatus_ =
   UpdateThingShadowResponse'
-    {_utsrsPayload = Nothing, _utsrsResponseStatus = pResponseStatus_}
-
+    { _utsrrsPayload =
+        Nothing,
+      _utsrrsResponseStatus = pResponseStatus_
+    }
 
 -- | The state information, in JSON format.
-utsrsPayload :: Lens' UpdateThingShadowResponse (Maybe ByteString)
-utsrsPayload = lens _utsrsPayload (\ s a -> s{_utsrsPayload = a})
+utsrrsPayload :: Lens' UpdateThingShadowResponse (Maybe ByteString)
+utsrrsPayload = lens _utsrrsPayload (\s a -> s {_utsrrsPayload = a})
 
 -- | -- | The response status code.
-utsrsResponseStatus :: Lens' UpdateThingShadowResponse Int
-utsrsResponseStatus = lens _utsrsResponseStatus (\ s a -> s{_utsrsResponseStatus = a})
+utsrrsResponseStatus :: Lens' UpdateThingShadowResponse Int
+utsrrsResponseStatus = lens _utsrrsResponseStatus (\s a -> s {_utsrrsResponseStatus = a})
 
-instance NFData UpdateThingShadowResponse where
+instance NFData UpdateThingShadowResponse
--- amazonka-iot-dataplane/test/Test/AWS/Gen/IoTData.hs
+++ amazonka-iot-dataplane/test/Test/AWS/Gen/IoTData.hs
@@ -1,16 +1,15 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 {-# OPTIONS_GHC -fno-warn-unused-imports #-}
-{-# OPTIONS_GHC -fno-warn-orphans        #-}
 
 -- Derived from AWS service descriptions, licensed under Apache 2.0.
 
 -- |
 -- Module      : Test.AWS.Gen.IoTData
--- Copyright   : (c) 2013-2018 Brendan Hay
+-- Copyright   : (c) 2013-2021 Brendan Hay
 -- License     : Mozilla Public License, v. 2.0.
 -- Maintainer  : Brendan Hay <brendan.g.hay+amazonka@gmail.com>
 -- Stability   : auto-generated
 -- Portability : non-portable (GHC extensions)
---
 module Test.AWS.Gen.IoTData where
 
 import Data.Proxy
@@ -31,14 +30,17 @@ import Test.Tasty
 --         [ requestGetThingShadow $
 --             getThingShadow
 --
---         , requestDeleteThingShadow $
---             deleteThingShadow
+--         , requestPublish $
+--             publish
+--
+--         , requestListNamedShadowsForThing $
+--             listNamedShadowsForThing
 --
 --         , requestUpdateThingShadow $
 --             updateThingShadow
 --
---         , requestPublish $
---             publish
+--         , requestDeleteThingShadow $
+--             deleteThingShadow
 --
 --           ]
 
@@ -46,14 +48,17 @@ import Test.Tasty
 --         [ responseGetThingShadow $
 --             getThingShadowResponse
 --
---         , responseDeleteThingShadow $
---             deleteThingShadowResponse
+--         , responsePublish $
+--             publishResponse
+--
+--         , responseListNamedShadowsForThing $
+--             listNamedShadowsForThingResponse
 --
 --         , responseUpdateThingShadow $
 --             updateThingShadowResponse
 --
---         , responsePublish $
---             publishResponse
+--         , responseDeleteThingShadow $
+--             deleteThingShadowResponse
 --
 --           ]
 --     ]
@@ -61,51 +66,73 @@ import Test.Tasty
 -- Requests
 
 requestGetThingShadow :: GetThingShadow -> TestTree
-requestGetThingShadow = req
+requestGetThingShadow =
+  req
     "GetThingShadow"
     "fixture/GetThingShadow.yaml"
 
-requestDeleteThingShadow :: DeleteThingShadow -> TestTree
-requestDeleteThingShadow = req
-    "DeleteThingShadow"
-    "fixture/DeleteThingShadow.yaml"
+requestPublish :: Publish -> TestTree
+requestPublish =
+  req
+    "Publish"
+    "fixture/Publish.yaml"
+
+requestListNamedShadowsForThing :: ListNamedShadowsForThing -> TestTree
+requestListNamedShadowsForThing =
+  req
+    "ListNamedShadowsForThing"
+    "fixture/ListNamedShadowsForThing.yaml"
 
 requestUpdateThingShadow :: UpdateThingShadow -> TestTree
-requestUpdateThingShadow = req
+requestUpdateThingShadow =
+  req
     "UpdateThingShadow"
     "fixture/UpdateThingShadow.yaml"
 
-requestPublish :: Publish -> TestTree
-requestPublish = req
-    "Publish"
-    "fixture/Publish.yaml"
+requestDeleteThingShadow :: DeleteThingShadow -> TestTree
+requestDeleteThingShadow =
+  req
+    "DeleteThingShadow"
+    "fixture/DeleteThingShadow.yaml"
 
 -- Responses
 
 responseGetThingShadow :: GetThingShadowResponse -> TestTree
-responseGetThingShadow = res
+responseGetThingShadow =
+  res
     "GetThingShadowResponse"
     "fixture/GetThingShadowResponse.proto"
     ioTData
     (Proxy :: Proxy GetThingShadow)
 
-responseDeleteThingShadow :: DeleteThingShadowResponse -> TestTree
-responseDeleteThingShadow = res
-    "DeleteThingShadowResponse"
-    "fixture/DeleteThingShadowResponse.proto"
+responsePublish :: PublishResponse -> TestTree
+responsePublish =
+  res
+    "PublishResponse"
+    "fixture/PublishResponse.proto"
     ioTData
-    (Proxy :: Proxy DeleteThingShadow)
+    (Proxy :: Proxy Publish)
+
+responseListNamedShadowsForThing :: ListNamedShadowsForThingResponse -> TestTree
+responseListNamedShadowsForThing =
+  res
+    "ListNamedShadowsForThingResponse"
+    "fixture/ListNamedShadowsForThingResponse.proto"
+    ioTData
+    (Proxy :: Proxy ListNamedShadowsForThing)
 
 responseUpdateThingShadow :: UpdateThingShadowResponse -> TestTree
-responseUpdateThingShadow = res
+responseUpdateThingShadow =
+  res
     "UpdateThingShadowResponse"
     "fixture/UpdateThingShadowResponse.proto"
     ioTData
     (Proxy :: Proxy UpdateThingShadow)
 
-responsePublish :: PublishResponse -> TestTree
-responsePublish = res
-    "PublishResponse"
-    "fixture/PublishResponse.proto"
+responseDeleteThingShadow :: DeleteThingShadowResponse -> TestTree
+responseDeleteThingShadow =
+  res
+    "DeleteThingShadowResponse"
+    "fixture/DeleteThingShadowResponse.proto"
     ioTData
-    (Proxy :: Proxy Publish)
+    (Proxy :: Proxy DeleteThingShadow)
