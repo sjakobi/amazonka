56e271b3d9dd7108bcd7eeb75800a34908612db1
Simon Jakobi <simon.jakobi@gmail.com>
2021-09-28 19:29:55 +0200

    Minimize N.A.D.ByteString

--- core/src/Network/AWS/Data/ByteString.hs
+++ core/src/Network/AWS/Data/ByteString.hs
@@ -18,34 +18,21 @@ module Network.AWS.Data.ByteString
     (
     -- * ByteString
       ByteString
-    , LazyByteString
     , ToByteString (..)
-    , showBS
-    , stripBS
     ) where
 
 import           Data.ByteString              (ByteString)
 import           Data.ByteString.Builder      (Builder)
 import qualified Data.ByteString.Char8        as BS8
 import qualified Data.ByteString.Lazy         as LBS
-import qualified Data.ByteString.Lazy.Builder as Build
-import           Data.CaseInsensitive         (CI)
-import qualified Data.CaseInsensitive         as CI
-import           Data.Char
+import qualified Data.ByteString.Builder      as Build
 import qualified Data.Text.Encoding           as Text
 import           Data.Time                    (UTCTime)
 import           Network.AWS.Data.Text
-import           Network.HTTP.Types
 import           Numeric
 import           Numeric.Natural
 
 type LazyByteString = LBS.ByteString
-
-showBS :: ToByteString a => a -> String
-showBS = BS8.unpack . toBS
-
-stripBS :: ByteString -> ByteString
-stripBS = BS8.dropWhile isSpace . fst . BS8.spanEnd isSpace
 
 class ToByteString a where
     toBS :: a -> ByteString
@@ -62,8 +49,4 @@ instance ToByteString Int            where toBS = toBS . Build.intDec
 instance ToByteString Integer        where toBS = toBS . Build.integerDec
 instance ToByteString Natural        where toBS = toBS . toInteger
 instance ToByteString Double         where toBS = toBS . ($ "") . showFFloat Nothing
-instance ToByteString StdMethod      where toBS = renderStdMethod
 instance ToByteString UTCTime        where toBS = BS8.pack . show
-
-instance ToByteString a => ToByteString (CI a) where
-    toBS = toBS . CI.original
