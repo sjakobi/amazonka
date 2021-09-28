8af1d74311b19bb435dfd15e32b765a67c679bd6
Simon Jakobi <simon.jakobi@gmail.com>
2021-09-28 17:53:08 +0200

    Minimize N.A.D.List1

--- core/src/Network/AWS/Data/List1.hs
+++ core/src/Network/AWS/Data/List1.hs
@@ -13,21 +13,16 @@
 -- Stability   : provisional
 -- Portability : non-portable (GHC extensions)
 --
-module Network.AWS.Data.List1 where
+module Network.AWS.Data.List1 (parseXMLList1, _List1, List1) where
 
 import           Control.DeepSeq
 import           Control.Monad
-import           Data.Aeson
 import           Data.Coerce
 import           Data.Data            (Data, Typeable)
-import           Data.Foldable        (Foldable)
-import qualified Data.Foldable        as Fold
 import           Data.Hashable
 import           Data.List.NonEmpty   (NonEmpty (..))
 import qualified Data.List.NonEmpty   as NonEmpty
-import           Data.Semigroup
 import           Data.Text            (Text)
-import           Data.Traversable     (Traversable, traverse)
 import           GHC.Exts
 import           GHC.Generics         (Generic)
 import           Network.AWS.Data.XML
@@ -61,17 +56,6 @@ instance IsList (List1 a) where
 
    fromList = List1 . NonEmpty.fromList
    toList   = NonEmpty.toList . toNonEmpty
-
-instance FromJSON a => FromJSON (List1 a) where
-    parseJSON = withArray "List1" (go >=> traverse parseJSON)
-      where
-        go = maybe (fail "Error parsing empty List1 when expecting at least one element.")
-                   (pure . List1)
-           . NonEmpty.nonEmpty
-           . Fold.toList
-
-instance ToJSON a => ToJSON (List1 a) where
-    toJSON = toJSON . toList
 
 instance Hashable a => Hashable (List1 a)
 
